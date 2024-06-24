5ef02b0fcccd048448c5f2852822ecae241b2764
AnnulusGames <annulusgames@gmail.com>
2024-06-24 17:34:31 +0900

    Add error message

--- src/Gyazo/GyazoApiException.cs
+++ src/Gyazo/GyazoApiException.cs
@@ -1,13 +1,36 @@
-using System.Net;
+using System.Text.Json.Serialization;
 
 namespace Gyazo;
 
-public sealed class GyazoApiException(HttpStatusCode status, string message) : Exception(message)
+public sealed class GyazoApiException(ErrorCode status, string? message) : Exception(message)
 {
-    public HttpStatusCode Status { get; } = status;
+    public ErrorCode Status { get; } = status;
 
     public override string ToString()
     {
         return $"{Status}: {Message}";
     }
+}
+
+public enum ErrorCode
+{
+    InvalidRequestError = 400,
+    AuthenticationError = 401,
+    PermissionError = 403,
+    NotFoundError = 404,
+    UnprocessableError = 422,
+    RateLimitError = 429,
+    ApiError = 500,
+}
+
+public record ErrorResponse
+{
+    [JsonPropertyName("message")]
+    public string? Message { get; init; }
+
+    [JsonPropertyName("request")]
+    public string? Request { get; init; }
+
+    [JsonPropertyName("method")]
+    public string? Method { get; init; }
 }
